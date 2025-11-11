d2ab0e822763df91978dc69047d654de9fbe560e
NGYW <yuwei9904@gmail.com>
2025-11-11 11:29:15 +0800

    Remove emoji from app title for consistency

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Lab Demo")
+st.title("Lab Demo")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
